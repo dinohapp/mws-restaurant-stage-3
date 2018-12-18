e7aff6b5f3bd3fe111145755d2c862e0f1f7ae67
Alex <dinohapp@gmail.com>
2018-12-17 19:14:08 -0500

    adjust code according to project reviewer notes

--- assets/js/main.js
+++ assets/js/main.js
@@ -144,7 +144,7 @@ const createRestaurantHTML = (restaurant) => {
   image.alt = DBHelper.imageAltForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
